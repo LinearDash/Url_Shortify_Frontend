de5fd9588bd2996b4e524c140e6edbce727fb60a
LinearDash <asandesh495@gmail.com>
2026-01-10 17:51:09 +0545

    Add Homepage component and routing setup
    - Created a new Homepage component with a welcome message.
    - Set up routing to render the Homepage at the root path.

---
+++ src/pages/homepage.tsx
@@ -0,0 +1,10 @@
+export const Homepage = () => {
+  return (
+    <div className="p-3 " >
+        <div className="flex items-center justify-center">
+            <h1 >Welcome to the Homepage</h1>
+        </div>
+      
+    </div>
+  );
+}
